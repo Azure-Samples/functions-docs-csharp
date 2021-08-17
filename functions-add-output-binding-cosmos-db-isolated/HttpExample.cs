0956d2e36b9bd5268d6ba33cb7706592b6a7ad41
Glenn Gailey <glenn.gailey@microsoft.com>
2021-08-17 10:23:02 -0700

    completed isolated example

--- functions-add-output-binding-cosmos-db-isolated/HttpExample.cs
+++ functions-add-output-binding-cosmos-db-isolated/HttpExample.cs
@@ -3,29 +3,45 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 
 namespace My.Functions
 {
     public static class HttpExample
     {
         [Function("HttpExample")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-        [CosmosDB(
-    databaseName: "my-database",
-    collectionName: "my-container",
-    ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
+        public static MultiResponse Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("HttpExample");
             logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            var message = "Welcome to Azure Functions!";
+
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString(message);
 
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
+            // Return a response to both HTTP trigger and Azure Cosmos DB output binding.
+            return new MultiResponse()
+            {
+                 Document = new MyDocument
+                {
+                    id = System.Guid.NewGuid().ToString(),
+                    message = message
+                },
+                HttpResponse = response
+            };
         }
+    }
+    public class MultiResponse
+    {
+        [CosmosDBOutput("my-database", "my-container",
+            ConnectionStringSetting = "CosmosDbConnectionString", CreateIfNotExists = true)]
+        public MyDocument Document { get; set; }
+        public HttpResponseData HttpResponse { get; set; }
+    }
+    public class MyDocument {
+        public string id { get; set; }
+        public string message { get; set; }
     }
 }
